5c6393c28bd7d1ed3f544da8461566c4dd8cb3e7
Lucas Loepke <wavyknight72@gmail.com>
2026-09-18 11:09:01 -0400

    round 2

--- task.py
+++ task.py
@@ -19,7 +19,7 @@ class Task(object):
     def t3(self):
         df_crosstab = self.t2()
         def get_percent(group):
-            x= (group / group['All'])
+            x= (group / group['All']).round(2)
             return x
 
         return df_crosstab.apply(get_percent, axis=1)
